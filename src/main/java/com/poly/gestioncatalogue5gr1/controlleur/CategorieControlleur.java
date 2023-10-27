87a953770217716abd89f654195f24c0715280d3
achref douss <achrefdouss@gmail.com>
2023-10-27 14:52:04 +0100

    new features: Add categorie & Edit Categorie

--- src/main/java/com/poly/gestioncatalogue5gr1/controlleur/CategorieControlleur.java
+++ src/main/java/com/poly/gestioncatalogue5gr1/controlleur/CategorieControlleur.java
@@ -1,11 +1,13 @@
 package com.poly.gestioncatalogue5gr1.controlleur;
 
 import com.poly.gestioncatalogue5gr1.entities.Categorie;
+import com.poly.gestioncatalogue5gr1.entities.Produit;
 import com.poly.gestioncatalogue5gr1.service.IServiceCategory;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,6 +29,25 @@ public class CategorieControlleur {
         serviceCategory.deleteCategorie(id);
         return "redirect:/index2";
     }
+    @GetMapping("/formcategorie")
+    public String formAjouter(Model m){
+            m.addAttribute("categorie",new Categorie());
+            return "ajoutCat";
+    }
 
+    @GetMapping("/add")
+    public String saveCategorie(@ModelAttribute Categorie c, Model m)
+    {
+        serviceCategory.saveCategory(c);
+        return "redirect:/index2";
+    }
+
+    @GetMapping("/editcat/{id}")
+    public String editCategorie(@PathVariable("id") Long id,Model m){
+
+        m.addAttribute("categorie",serviceCategory.getCategorie(id));
+
+        return "ajoutCat";
+    }
 
 }
